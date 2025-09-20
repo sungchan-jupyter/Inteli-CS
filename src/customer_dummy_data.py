827b653ead62a0a425a1d443266f5c4d024f2465
sungchan-jupyter <holypraise83@gmail.com>
2025-09-20 18:03:39 +0900

    feat: Add customer and order dummy data for testing
    - Sample customer data with Korean customer information
    - Sample order data with delivery status and timing
    - Test data for dispatch delay scenarios (36 minutes wait)
    - Menu items and restaurant information included

---
+++ src/customer_dummy_data.py
@@ -0,0 +1,48 @@
+CUSTOMER_DATA = {
+   "cust_no":"1001",
+   "cust_id":"kim.cs@example.com",
+   "cust_nm":"김철수",
+   "join_dttm":"2024-09-19T13:42:14.935542",
+   "first_ord_dttm":"2024-10-04T13:42:14.935565",
+   "grade_cd":"A",
+   "cs_flag":"M"
+}
+
+ORDER_DATA = {
+   "ord_no":"ORD002",
+   "cust_no":"1002",
+   "latest_dlvry_fee":4500,
+   "gap_dlvry_fee":500,
+   "ord_amt":25000,
+   "expect_dlvry_fee":4000,
+   "auto_cancel_avail_yn":False,
+   "auto_defense_avail_yn":True,
+   "ord_dttm":"2025-09-19T12:57:14.963728",
+   "cancel_dttm":None,
+   "expect_cook_cmplt_dttm":None,
+   "cust_noti_dttm":"2025-09-19T13:00:14.963728",
+   "dispatch_start_dttm":"2025-09-19T13:07:14.963728",
+   "dispatch_cmplt_dttm":None,
+   "dispatch_wait_mncnt":36,
+   "cook_cmplt_dttm":None,
+   "food_wait_mncnt":None,
+   "arrived_shop_dttm":None,
+   "pickup_dttm":None,
+   "handover_dttm":None,
+   "delay_mncnt":43,
+   "status":"DISPATCH_WAITING",
+   "shop_nm":"피자헛 서초점",
+   "shop_cate_nm":"피자",
+   "shop_addrs":"서울시 서초구 서초대로 200",
+   "shop_phone_no":"02-2345-6789",
+   "handover_addrs":"서울시 서초구 서초대로 456",
+   "handover_phone_no":"010-2345-6789",
+   "menu_items":[
+      {
+         "food_nm":"페퍼로니 피자(L)",
+         "food_price":25000,
+         "food_quantity":1,
+         "food_amt":25000
+      }
+   ]
+}
